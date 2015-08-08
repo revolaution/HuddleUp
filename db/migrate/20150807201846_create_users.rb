0a00f128ddacc0715595340f6f5d29f46e7f7eae
Ryan Lau & Brian Fu <pair+bastonis.fu1two@devbootcamp.com>
2015-08-08 15:37:29 -0500

    Add location reference. Adjust seed file.

--- db/migrate/20150807201846_create_users.rb
+++ db/migrate/20150807201846_create_users.rb
@@ -6,6 +6,7 @@ class CreateUsers < ActiveRecord::Migration
       t.string :name
       t.string :email
       t.string :image
+      t.references :location
 
       t.timestamps null: false
     end
