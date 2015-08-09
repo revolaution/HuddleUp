774fc57defee8d6116bace4731ff546c1bb499ef
Ryan Lau <ryanlau92@gmail.com>
2015-08-09 11:59:13 -0500

    Refactored crud routes, created partials, cleared up routes

--- app/controllers/participatings_controller.rb
+++ app/controllers/participatings_controller.rb
@@ -1,6 +1,21 @@
 class ParticipatingsController < ApplicationController
-  def new
-    Participating.create(participant: current_user, game: Game.find(params[:game_id]))
-    redirect_to location_sport_game_path(id: params[:game_id])
+  before_action :load_location, :load_sport, :load_game
+
+  def create
+    Participating.create(participant: current_user, game: @game)
+    redirect_to location_sport_game_path(@location, @sport, @game)
   end
+
+  private
+    def load_location
+      @location = Location.find(params[:location_id])
+    end
+
+    def load_sport
+      @sport = Sport.find(params[:sport_id])
+    end
+
+    def load_game
+      @game = Game.find(params[:game_id])
+    end
 end
