6072b558289e47fc751bd497ae31afb61cc7324d
Brian Fu & Nicholas Stelter <pair+fu1two.steltz@devbootcamp.com>
2015-08-09 14:38:18 -0500

    update seeds to include 9 sports

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,8 +10,15 @@ Location.destroy_all
 Sport.destroy_all
 Participating.destroy_all
 Game.destroy_all
+
 user_1 = User.first
 chicago = Location.create(name: "Chicago")
-sport_1 = Sport.create(name: "Hockey", description: "theres a puck")
-game_1 = Game.create(description: "Hockey Game 1", creator: user_1, sport: sport_1, location: chicago, address: "soldier field")
-Participating.create(participant: user_1, game: game_1)
+sport_1 = Sport.create(name: "Basketball", description: "Ex choro perfecto sed, ei laoreet atomorum convenire vim. Est soluta laoreet deterruisset ad. At sea paulo libris, id usu paulo simul, in mel oratio malorum delenit. Cu dicant philosophia cum. Ad eos oblique pertinacia.")
+sport_2 = Sport.create(name: "Volleyball", description: "Ex choro perfecto sed, ei laoreet atomorum convenire vim. Est soluta laoreet deterruisset ad. At sea paulo libris, id usu paulo simul, in mel oratio malorum delenit. Cu dicant philosophia cum. Ad eos oblique pertinacia.")
+sport_3 = Sport.create(name: "Football", description: "Ex choro perfecto sed, ei laoreet atomorum convenire vim. Est soluta laoreet deterruisset ad. At sea paulo libris, id usu paulo simul, in mel oratio malorum delenit. Cu dicant philosophia cum. Ad eos oblique pertinacia.")
+sport_4 = Sport.create(name: "Ultimate Frisbee", description: "Ex choro perfecto sed, ei laoreet atomorum convenire vim. Est soluta laoreet deterruisset ad. At sea paulo libris, id usu paulo simul, in mel oratio malorum delenit. Cu dicant philosophia cum. Ad eos oblique pertinacia.")
+sport_5 = Sport.create(name: "Soccer", description: "Ex choro perfecto sed, ei laoreet atomorum convenire vim. Est soluta laoreet deterruisset ad. At sea paulo libris, id usu paulo simul, in mel oratio malorum delenit. Cu dicant philosophia cum. Ad eos oblique pertinacia.")
+sport_6 = Sport.create(name: "Tennis", description: "Ex choro perfecto sed, ei laoreet atomorum convenire vim. Est soluta laoreet deterruisset ad. At sea paulo libris, id usu paulo simul, in mel oratio malorum delenit. Cu dicant philosophia cum. Ad eos oblique pertinacia.")
+sport_7 = Sport.create(name: "Hockey", description: "Ex choro perfecto sed, ei laoreet atomorum convenire vim. Est soluta laoreet deterruisset ad. At sea paulo libris, id usu paulo simul, in mel oratio malorum delenit. Cu dicant philosophia cum. Ad eos oblique pertinacia.")
+sport_8 = Sport.create(name: "Baseball", description: "Ex choro perfecto sed, ei laoreet atomorum convenire vim. Est soluta laoreet deterruisset ad. At sea paulo libris, id usu paulo simul, in mel oratio malorum delenit. Cu dicant philosophia cum. Ad eos oblique pertinacia.")
+sport_9 = Sport.create(name: "Miscellaneous", description: "Ex choro perfecto sed, ei laoreet atomorum convenire vim. Est soluta laoreet deterruisset ad. At sea paulo libris, id usu paulo simul, in mel oratio malorum delenit. Cu dicant philosophia cum. Ad eos oblique pertinacia.")
