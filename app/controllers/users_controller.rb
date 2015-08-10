50f48125a8119874535b286d62d3898c966091e3
Michael Ramuta <michaelramuta@gmail.com>
2015-08-10 10:10:00 -0500

    user can change default city

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,22 @@
 class UsersController < ApplicationController
   def index
   end
+
+  def show
+    @locations = Location.all
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    redirect_to @user unless @user == current_user
+    @user.location = Location.find(city_params["location_id"])
+    @user.save
+    redirect_to @user.location
+  end
+
+  private
+  def city_params
+    params.require(:user).permit(:location_id)
+  end
 end
