100168267876f464d7bf5ea83cb76d4abd444932
Michael Ramuta <michaelramuta@gmail.com>
2015-08-11 17:35:16 -0500

    main logo to link user page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150811204735) do
+ActiveRecord::Schema.define(version: 20150811221021) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,10 +54,9 @@ ActiveRecord::Schema.define(version: 20150811204735) do
   end
 
   create_table "sports", force: :cascade do |t|
-    t.string   "name",        null: false
-    t.string   "description", null: false
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.string   "name",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "teams", force: :cascade do |t|
