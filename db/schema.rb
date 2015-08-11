3e8aaa0e1a2323d3f98f056ac4bfda398b7a360a
Fu1two <brian.fu.uxd@gmail.com>
2015-08-11 17:38:15 -0500

    Scema udpate.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150811204735) do
+ActiveRecord::Schema.define(version: 20150811221021) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,9 +55,8 @@ ActiveRecord::Schema.define(version: 20150811204735) do
 
   create_table "sports", force: :cascade do |t|
     t.string   "name"
-    t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "teams", force: :cascade do |t|
