113b325455b2cdf47af910edea5cfaafda931b17
Fu1two <brian.fu.uxd@gmail.com>
2015-08-11 16:57:53 -0500

    Move calendar and made it auto submit.

--- app/assets/javascripts/materialize.js
+++ app/assets/javascripts/materialize.js
@@ -3,6 +3,10 @@ $(document).ready(function(){
   	$(".edit_user").submit();
   });
 
+  $("#sport_new_date").change(function(){
+  	$(".edit_sport").submit();
+  });
+
   $('.collapsible').collapsible({
       accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
     });
