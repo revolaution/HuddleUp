34aba43d4b2e92ad3272209c24669959db35a110
Nicholas Stelter <nicholas.stelter@gmail.com>
2015-08-11 08:35:49 -0500

    Move JS map file from vendor/assets to app/assets

---
+++ app/assets/javascripts/maps.js
@@ -0,0 +1,6 @@
+function initialize() {
+  var input = document.getElementById('searchTextField');
+  var autocomplete = new google.maps.places.Autocomplete(input);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
