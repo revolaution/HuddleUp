a1d85048b21283d67d7b8aa4ea0206031ed4513d
Ryan Lau <ryanlau92@gmail.com>
2015-08-12 13:56:36 -0500

    took out a route that wasn't needed

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
 
   # You can have the root of your site routed with "root"
-  resources :locations, only: [:show, :new, :post] do
+  resources :locations, only: [:show, :new] do
     resources :sports, only: [:show] do
       resources :games, except: [:index] do
         resources :participatings, only: [:create ]
