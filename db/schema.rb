42f0b89b10442a08781c461e857d2bc90de51bfc
Nicholas Stelter <nicholas.stelter@gmail.com>
2015-08-12 13:13:34 -0500

    style css sport show page

--- db/schema.rb
+++ db/schema.rb
@@ -54,7 +54,7 @@ ActiveRecord::Schema.define(version: 20150812145815) do
   end
 
   create_table "sports", force: :cascade do |t|
-    t.string   "name"
+    t.string   "name",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "image_path"
