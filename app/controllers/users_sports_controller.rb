1d22deca3a4f17948af721ebfb4a949daedcb520
Nicholas Stelter <nicholas.stelter@gmail.com>
2015-08-12 10:53:29 -0500

    update skill filter to stay on same date after selection

--- app/controllers/users_sports_controller.rb
+++ app/controllers/users_sports_controller.rb
@@ -1,13 +1,14 @@
 class UsersSportsController < ApplicationController
 
   def change_skill
+    filter_date = params[:format]
     @location = Location.find(params[:location_id])
     @sport = Sport.find(params[:id])
     redirect_to @location unless current_user
     @skill = UsersSport.find_by(sport: @sport, user: current_user)
     @skill.skill_level = skill_params["skill_level"]
     @skill.save
-    redirect_to location_sport_path(@location, @sport)
+    redirect_to "/locations/#{@location.id}/sports/#{@sport.id}?date=#{filter_date}"
   end
 
   private
