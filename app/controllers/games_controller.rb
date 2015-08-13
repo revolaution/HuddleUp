626905c8f2d061cc72d2d9b0f13ad51f49b68bae
Michael Ramuta <michaelramuta@gmail.com>
2015-08-13 09:22:31 -0500

    form remembers start at and end at times

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -15,6 +15,13 @@ class GamesController < ApplicationController
     redirect_to_sport(@location, @sport) unless current_user
     @date = params[:date]
     @game = Game.new
+    unless @game.start_at
+      @game.start_at = (Time.now + 1.hours).beginning_of_hour.strftime("%H:%M")
+    end
+    unless @game.end_at
+      @game.end_at = (Time.now + 3.hours).beginning_of_hour.strftime("%H:%M")
+    end
+
     if request.xhr?
       render 'new', layout: false
     end
